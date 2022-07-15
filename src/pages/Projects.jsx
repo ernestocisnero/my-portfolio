9c1c2d733c63c7e419f6b4be36d16eff04e8da22
ErnestoCisnero <miguelernestocisnero@gmail.com>
2022-07-15 15:32:58 -0400

    added electro-shop project

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -15,25 +15,11 @@ export const Projects = () => {
                 />
 
                 <Card
-                    ImgPath={"/assets/images/coding-screen.jpg"}
-                    projectTitle={"Example:"}
-                    projectDescription={"Example text..."}
-                    urlProject={"https://google.com"}
-                    urlRepo={"https://google.com"} 
-                    />
-                <Card
-                    ImgPath={"/assets/images/coding-screen.jpg"}
-                    projectTitle={"Example:"}
-                    projectDescription={"Example text..."}
-                    urlProject={"https://google.com"}
-                    urlRepo={"https://google.com"} 
-                    />
-                <Card
-                    ImgPath={"/assets/images/coding-screen.jpg"}
-                    projectTitle={"Example:"}
-                    projectDescription={"Example text..."}
-                    urlProject={"https://google.com"}
-                    urlRepo={"https://google.com"} 
+                    ImgPath={"/assets/images/electro-shop.png"}
+                    projectTitle={"Electro-Shop:"}
+                    projectDescription={"Online shop, where you can find hardware products to develop you next automation project."}
+                    urlProject={"https://electroshopp.netlify.app/"}
+                    urlRepo={"https://github.com/ernestocisnero/electroShop"} 
                     />
                 <Card
                     ImgPath={"/assets/images/coding-screen.jpg"}
