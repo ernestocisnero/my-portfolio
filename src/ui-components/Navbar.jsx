66dd10bdd62c013b4c13854befa033da3dd10cdd
ErnestoCisnero <miguelernestocisnero@gmail.com>
2022-07-16 19:06:54 -0400

    responsive nabvar toggle behavior

--- src/ui-components/Navbar.jsx
+++ src/ui-components/Navbar.jsx
@@ -2,44 +2,52 @@ import { NavLink } from "react-router-dom";
 
 import "./style.css";
 
-export const Navbar = () => {   
+export const Navbar = () => {
 
     return (
         <>
-            <nav className="navbar d-flex flex-nowrap navbar-expand-lg navbar-dark bg-dark px-3 text-uppercase">
-                <div className="container mb-2">
-                    <NavLink className="nav-item nav-link" to="/">
-                        <b>Ernesto Cisnero</b>
-                    </NavLink>
-                </div>
+            <nav className="navbar row d-flex flex-nowrap justify-content-between navbar-expand-lg navbar-dark bg-dark px-3 text-uppercase">
 
-                <button
-                    className="navbar-toggler"
-                    type="button"
-                    data-bs-toggle="collapse"
-                    data-bs-target="#navbarToggler"
-                    aria-controls="navbarToggler"
-                    aria-expanded="false"
-                    aria-label="Toggle navigation"
-                >
-                    <span class="navbar-toggler-icon"></span>
-                </button>
+                    <div className="col-6">
+                        <div className="container mb-2">
+                            <NavLink className="nav-item nav-link" to="/">
+                                <b>Ernesto Cisnero</b>
+                            </NavLink>
+                        </div>
 
-                <div className="collapse navbar-collapse" id="navbarToggler">
-                    <div className="navbar-nav ">
-                        <NavLink className="nav-item nav-link underline" to="/">
-                            About
-                        </NavLink>
-
-                        <NavLink className="nav-item nav-link underline" to="/projects">
-                            Projects
-                        </NavLink>
-
-                        <NavLink className="nav-item nav-link underline" to="/contact">
-                            Contact
-                        </NavLink>
                     </div>
-                </div>
+
+                    <div className="col-6 mx-5">
+                        <button
+                            className="navbar-toggler"
+                            type="button"
+                            data-bs-toggle="collapse"
+                            data-bs-target="#navbarToggler"
+                            aria-controls="navbarToggler"
+                            aria-expanded="false"
+                            aria-label="Toggle navigation"
+                        >
+                            <span className="navbar-toggler-icon"></span>
+                        </button>
+
+                        <div className="collapse navbar-collapse" id="navbarToggler">
+                            <div className="navbar-nav ">
+                                <NavLink className="nav-item nav-link underline" to="/">
+                                    About
+                                </NavLink>
+
+                                <NavLink className="nav-item nav-link underline" to="/projects">
+                                    Projects
+                                </NavLink>
+
+                                <NavLink className="nav-item nav-link underline" to="/contact">
+                                    Contact
+                                </NavLink>
+                            </div>
+                        </div>
+
+                    </div>
+                
             </nav>
         </>
     );
